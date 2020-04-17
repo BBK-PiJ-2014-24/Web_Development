c999edad617486dae7fac40e4cb36bc69e5d10fb
git newnham <git@newnham.com>
2020-04-17 22:40:28 +0800

    break-up project into different projects

--- Sequelize_Model/app.js
+++ Sequelize_Model/app.js
@@ -1,29 +1,12 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "movies.db",
-  logging: true,
-}); // storage ref to database; logging logs all insertions/queries to console
-
-// Create a table/model class that extends Sequelize.Model
-class Movie extends Sequelize.Model {}
-
-// Initialize the table/model schema with title column
-Movie.init(
-  {
-    title: Sequelize.STRING,
-  },
-  { sequelize }
-);
+const db = require("./db");
+const { Movie } = db.models;
 
 // async IIFE
 (async () => {
-  await sequelize.sync({ force: true }); // method, which automatically creates or updates your database tables
-  // Calling sync({ force: true }) issues a DROP TABLE IF EXISTS statement, which completely deletes the table, before issuing the CREATE TABLE IF NOT EXISTS statement. In other words, it will drop a table that exists, each time you start your app, and recreate it from the model definition.
+  await db.sequelize.sync({ force: true }); // method, which automatically creates or updates your database tables
   try {
-    await sequelize.authenticate(); // test the connection to the db
-    console.log("Connection to the database successful!");
+    //await sequelize.authenticate(); // test the connection to the db
+    // console.log("Connection to the database successful!");
 
     const movieRows = await Promise.all([
       await Movie.create({
@@ -35,6 +18,7 @@ Movie.init(
       }),
     ]);
     const moviesJSON = movieRows.map((m) => m.toJSON());
+    console.log(moviesJSON);
   } catch (error) {
     console.error("Error connecting to the database: ", error);
   }
---
+++ Sequelize_Model/db/models/movie.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize) => {
+  class Movie extends Sequelize.Model {}
+
+  Movie.init(
+    {
+      title: Sequelize.STRING,
+    },
+    { sequelize }
+  );
+
+  return Movie;
+};
