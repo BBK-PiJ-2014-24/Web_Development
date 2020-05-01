a74d417424e5da74ce5f40b53da2b732b60addc0
git newnham <git@newnham.com>
2020-05-01 12:12:50 +0800

    first test of mocha on textUtilities

--- Unit_Testing/textUtilities.js
+++ Unit_Testing/textUtilities.js
@@ -1,3 +1,16 @@
+// npm i --save-dev mocha chai
+
 var expect = require("chai").expect;
 
+function titelCasefn(title) {
+  var words = title.split(" ");
+  var new_words = words.map((w) => w[0].toUpperCase() + w.substring(1));
+  return new_words.join(" ");
+}
+
 expect(titelCasefn("the great mouse detective")).to.be.a("string");
+expect(titelCasefn("a")).is.equal("A");
+expect(titelCasefn("vertigo")).to.equal("Vertigo");
+expect(titelCasefn("the great mouse detective")).is.equal(
+  "The Great Mouse Detective"
+);
