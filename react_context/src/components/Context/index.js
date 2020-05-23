96cf4f7a12bc7b589aa923787f165495069567e0
BBK-PiJ-2014-24 <git@newnham.com>
2020-05-23 11:12:23 +0800

    Covert Provider to a class including all global state. <Provider> is placed as the top component in the App, located in src/index.js. Appnow becomes a stateless function

--- react_context/src/components/Context/index.js
+++ react_context/src/components/Context/index.js
@@ -1,8 +1,83 @@
-import React from "react";
-
+import React, { Component } from "react";
 // Instantiate a CreateContext object
 const ScoreboardContext = React.createContext();
 
-// export the Proider and Consumer objects
-export const Provider = ScoreboardContext.Provider;
+// export the Proider and Consumer as Higher Order Classes
+// Provider is now in Src/index.js and becomes the top component.
+// Wrap the return with Provider element passing prop as an object of attributes, where
+// functions are passed in sub object called actions
+// this.props.children is a special React prop that passes actual components to other components
+export class Provider extends Component {
+  state = {
+    players: [
+      {
+        name: "Guil",
+        score: 0,
+        id: 1,
+      },
+      {
+        name: "Treasure",
+        score: 0,
+        id: 2,
+      },
+      {
+        name: "Ashley",
+        score: 0,
+        id: 3,
+      },
+      {
+        name: "James",
+        score: 0,
+        id: 4,
+      },
+    ],
+  };
+
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => ({
+      score: (prevState.players[index].score += delta),
+    }));
+  };
+
+  // player id counter
+  prevPlayerId = 4;
+  handleAddPlayer = (name) => {
+    this.setState((prevState) => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: (this.prevPlayerId += 1),
+          },
+        ],
+      };
+    });
+  };
+
+  handleRemovePlayer = (id) => {
+    this.setState((prevState) => {
+      return {
+        players: prevState.players.filter((p) => p.id !== id),
+      };
+    });
+  };
+  render() {
+    return (
+      <ScoreboardContext.Provider
+        value={{
+          players: this.state.players,
+          actions: {
+            changeScore: this.handleScoreChange,
+            removePlayer: this.handleRemovePlayer,
+            addPlayer: this.handleAddPlayer,
+          },
+        }}
+      >
+        {this.props.children}
+      </ScoreboardContext.Provider>
+    );
+  }
+}
 export const Consumer = ScoreboardContext.Consumer;
