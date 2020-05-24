9b3373cc9f83750279161113daca4363f64728d0
BBK-PiJ-2014-24 <git@newnham.com>
2020-05-24 16:34:33 +0800

    Set up a ErrorBoundary Class to globally handle errors through the Dom

--- react_error/src/App.js
+++ react_error/src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-import StudentForm from './components/StudentForm';
+import StudentForm from "./components/StudentForm";
 
+// Error Boundary allows u to wrap a container so only that part fails and the rest of the
+// page renders
 class App extends Component {
-  
   render() {
     return (
       <div className="App">
@@ -13,8 +15,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
-        <StudentForm />
+        <ErrorBoundary>
+          <StudentForm />
+        </ErrorBoundary>
       </div>
     );
   }
---
+++ react_error/src/components/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+//import { sendError } from "../error-config";
+
+// Error Boundary allows u to wrap a container so only that part fails and the rest of the
+// page renders
+export default class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+  };
+
+  // To catch Errors use 'componentDidCatch()' function
+  componentDidCatch(err, info) {
+    this.setState({ hasError: true });
+    // sendError.captureException(err, { extra: info });
+  }
+  render() {
+    if (this.state.hasError) {
+      return <h2>This is my ErrorBoundary Msg </h2>;
+    } else {
+      return this.props.children;
+    }
+  }
+}
