384346b2365be096c5805990c6a96ed960ff3ac5
BBK-PiJ-2014-24 <git@newnham.com>
2020-05-24 10:24:14 +0800

    Added Redirects to Courses

--- react_router/src/components/App.js
+++ react_router/src/components/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
 
+// Use BrowserRouter Component to contain all routes
+// Use Route component for each route
+// If there are no props, use component={}
+// If using props, use a render={component +props}
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" render={() => <About title="About Title" />} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
